de602ebf09a89105c9788beeec035476847d47eb
DhirenDev <dhiren5866@yahoo.com>
2024-06-25 22:29:10 +0530

    Adding Ordering Models

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainevents = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainevents.AsReadOnly();
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] domainEvents = _domainevents.ToArray();
+            _domainevents.Clear();
+            return domainEvents;
+        }
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainevents.Add(domainEvent);
+        }
+    }
+}
